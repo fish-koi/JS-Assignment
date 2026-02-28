fbc3a926544fc8ea6e70d0cd3947567955ced93d
alla <angelica.ae.epshtein@gmail.com>
2026-02-28 16:38:09 -0500

    started javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+class TicTacToe {
+    constructor(r1c1, r1c2, r1c3, r2c1, r2c2, r2c3, r3c1, r3c2, r3c3, gamesPlayed, gamesWon, gamesLost) {
+        this.r1c1 = r1c1;
+        this.r1c2 = r1c2;
+        this.r1c3 = r1c3;
+        this.r2c1 = r2c1;
+        this.r2c2 = r2c2;
+        this.r2c3 = r2c3;
+        this.r3c1 = r3c1;
+        this.r3c2 = r3c2;
+        this.r3c3 = r3c3;
+        this.gamesPlayed = gamesPlayed;
+        this.gamesWon = gamesWon;
+        this.gamesLost = gamesLost;
+    }
+
+}
 
 
-window.addEventListener("load", function (){
-    
+
+window.addEventListener("load", function () {
+    game = new TicTacToe("","","","","","","","","",0,0,0);
 });
