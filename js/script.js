456433bb9707f30eb78731da5a47bf6a978118c5
alla <angelica.ae.epshtein@gmail.com>
2026-02-28 19:07:09 -0500

    implemented some game mechanics

--- js/script.js
+++ js/script.js
@@ -1,23 +1,150 @@
-class TicTacToe {
-    constructor(r1c1, r1c2, r1c3, r2c1, r2c2, r2c3, r3c1, r3c2, r3c3, gamesPlayed, gamesWon, gamesLost) {
-        this.r1c1 = r1c1;
-        this.r1c2 = r1c2;
-        this.r1c3 = r1c3;
-        this.r2c1 = r2c1;
-        this.r2c2 = r2c2;
-        this.r2c3 = r2c3;
-        this.r3c1 = r3c1;
-        this.r3c2 = r3c2;
-        this.r3c3 = r3c3;
-        this.gamesPlayed = gamesPlayed;
-        this.gamesWon = gamesWon;
-        this.gamesLost = gamesLost;
-    }
-
-}
-
-
-
 window.addEventListener("load", function () {
-    game = new TicTacToe("","","","","","","","","",0,0,0);
+    class TicTacToe {
+        constructor(moves, gamesPlayed, gamesWon, gamesLost) {
+            this.moves = moves;
+            this.gamesPlayed = gamesPlayed;
+            this.gamesWon = gamesWon;
+            this.gamesLost = gamesLost;
+        }
+
+        makeMove(index) {
+            if (this.moves[index] == "") {
+                this.moves[index] = "X";
+                yourTurn = false;
+            }
+        }
+
+        computerMove() {
+            let index = Math.floor(Math.random() * (8))
+            while (this.moves[index] != "") {
+                index = Math.floor(Math.random() * (8));
+            }
+            this.moves[index] = "O";
+            setTimeout(() => {this.checkMoves();}, 1000);
+            setTimeout(() => {yourTurn = true;}, 1100);
+        }
+
+        checkMoves(){
+            if(this.moves[0] == "O"){
+                r1c1Move.innerHTML = "O";
+            }
+            if(this.moves[1] == "O"){
+                r1c2Move.innerHTML = "O";
+            }
+            if(this.moves[2] == "O"){
+                r1c3Move.innerHTML = "O";
+            }
+            if(this.moves[3] == "O"){
+                r2c1Move.innerHTML = "O";
+            }
+            if(this.moves[4] == "O"){
+                r2c2Move.innerHTML = "O";
+            }
+            if(this.moves[5] == "O"){
+                r2c3Move.innerHTML = "O";
+            }
+            if(this.moves[6] == "O"){
+                r3c1Move.innerHTML = "O";
+            }
+            if(this.moves[7] == "O"){
+                r3c2Move.innerHTML = "O";
+            }
+            if(this.moves[8] == "O"){
+                r3c3Move.innerHTML = "O";
+            }
+        }
+    }
+    let yourTurn = true;
+    game = new TicTacToe(["", "", "", "", "", "", "", "", ""], 0, 0, 0);
+
+    const r1c1Move = document.getElementById("r1-c1-move");
+    const r1c2Move = document.getElementById("r1-c2-move");
+    const r1c3Move = document.getElementById("r1-c3-move");
+    const r2c1Move = document.getElementById("r2-c1-move");
+    const r2c2Move = document.getElementById("r2-c2-move");
+    const r2c3Move = document.getElementById("r2-c3-move");
+    const r3c1Move = document.getElementById("r3-c1-move");
+    const r3c2Move = document.getElementById("r3-c2-move");
+    const r3c3Move = document.getElementById("r3-c3-move");
+
+    document.getElementById("r1-c1").addEventListener("click", function () {
+        if (yourTurn) {
+            game.makeMove(0);
+            r1c1Move.innerHTML = "X";
+            yourTurn = false;
+            game.computerMove();
+        }
+    });
+
+    document.getElementById("r1-c2").addEventListener("click", function () {
+        if (yourTurn) {
+            game.makeMove(1);
+            r1c2Move.innerHTML = "X";
+            yourTurn = false;
+            game.computerMove();
+        }
+    });
+
+    document.getElementById("r1-c3").addEventListener("click", function () {
+        if (yourTurn) {
+            game.makeMove(2);
+            r1c3Move.innerHTML = "X";
+            yourTurn = false;
+            game.computerMove();
+        }
+    });
+
+    document.getElementById("r2-c1").addEventListener("click", function () {
+        if (yourTurn) {
+            game.makeMove(3);
+            r2c1Move.innerHTML = "X";
+            yourTurn = false;
+            game.computerMove();
+        }
+    });
+
+    document.getElementById("r2-c2").addEventListener("click", function () {
+        if (yourTurn) {
+            game.makeMove(4);
+            r2c2Move.innerHTML = "X";
+            yourTurn = false;
+            game.computerMove();
+        }
+    });
+
+    document.getElementById("r2-c3").addEventListener("click", function () {
+        if (yourTurn) {
+            game.makeMove(5);
+            r2c3Move.innerHTML = "X";
+            yourTurn = false;
+            game.computerMove();
+        }
+    });
+
+    document.getElementById("r3-c1").addEventListener("click", function () {
+        if (yourTurn) {
+            game.makeMove(6);
+            r3c1Move.innerHTML = "X";
+            yourTurn = false;
+            game.computerMove();
+        }
+    });
+
+    document.getElementById("r3-c2").addEventListener("click", function () {
+        if (yourTurn) {
+            game.makeMove(7);
+            r3c2Move.innerHTML = "X";
+            yourTurn = false;
+            game.computerMove();
+        }
+    });
+
+    document.getElementById("r3-c3").addEventListener("click", function () {
+        if (yourTurn) {
+            game.makeMove(8);
+            r3c3Move.innerHTML = "X";
+            yourTurn = false;
+            game.computerMove();
+        }
+    });
 });
