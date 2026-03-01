b17616287d20d6c03524d2bf8634458948c8a96b
alla <angelica.ae.epshtein@gmail.com>
2026-03-01 15:09:50 -0500

    added win/lose checking

--- js/script.js
+++ js/script.js
@@ -1,61 +1,137 @@
 window.addEventListener("load", function () {
     class TicTacToe {
-        constructor(moves, gamesPlayed, gamesWon, gamesLost) {
+        constructor(moves, gamesPlayed, gamesWon, gamesLost, yourTurn) {
             this.moves = moves;
             this.gamesPlayed = gamesPlayed;
             this.gamesWon = gamesWon;
             this.gamesLost = gamesLost;
+            this.yourTurn = yourTurn;
         }
 
         makeMove(index) {
             if (this.moves[index] == "") {
                 this.moves[index] = "X";
-                yourTurn = false;
+                this.yourTurn = false;
+                this.checkWin();
             }
         }
 
         computerMove() {
-            let index = Math.floor(Math.random() * (8))
-            while (this.moves[index] != "") {
-                index = Math.floor(Math.random() * (8));
+            if (!xWin && !oWin) {
+                let index = Math.floor(Math.random() * (9))
+                while (this.moves[index] != "") {
+                    index = Math.floor(Math.random() * (9));
+                }
+                this.moves[index] = "O";
+                setTimeout(() => {
+                    this.checkMoves();
+                    this.checkWin();
+                    if (!xWin && !oWin) {
+                        this.yourTurn = true;
+                    }
+                }, 1000);
             }
-            this.moves[index] = "O";
-            setTimeout(() => {this.checkMoves();}, 1000);
-            setTimeout(() => {yourTurn = true;}, 1100);
         }
 
-        checkMoves(){
-            if(this.moves[0] == "O"){
-                r1c1Move.innerHTML = "O";
+        checkMoves() {
+            const squares = [r1c1Move, r1c2Move, r1c3Move,
+                r2c1Move, r2c2Move, r2c3Move,
+                r3c1Move, r3c2Move, r3c3Move];
+
+            for (let i = 0; i < this.moves.length; i++) {
+                squares[i].innerHTML = this.moves[i];
             }
-            if(this.moves[1] == "O"){
-                r1c2Move.innerHTML = "O";
+        }
+
+        checkWin() {
+            //checks all winning conditions for X
+            if (this.moves[0] == "X" && this.moves[1] == "X" && this.moves[2] == "X") {
+                xWin = true;
             }
-            if(this.moves[2] == "O"){
-                r1c3Move.innerHTML = "O";
+            if (this.moves[3] == "X" && this.moves[4] == "X" && this.moves[5] == "X") {
+                xWin = true;
             }
-            if(this.moves[3] == "O"){
-                r2c1Move.innerHTML = "O";
+            if (this.moves[6] == "X" && this.moves[7] == "X" && this.moves[8] == "X") {
+                xWin = true;
             }
-            if(this.moves[4] == "O"){
-                r2c2Move.innerHTML = "O";
+            if (this.moves[0] == "X" && this.moves[3] == "X" && this.moves[6] == "X") {
+                xWin = true;
             }
-            if(this.moves[5] == "O"){
-                r2c3Move.innerHTML = "O";
+            if (this.moves[1] == "X" && this.moves[4] == "X" && this.moves[7] == "X") {
+                xWin = true;
             }
-            if(this.moves[6] == "O"){
-                r3c1Move.innerHTML = "O";
+            if (this.moves[2] == "X" && this.moves[5] == "X" && this.moves[8] == "X") {
+                xWin = true;
             }
-            if(this.moves[7] == "O"){
-                r3c2Move.innerHTML = "O";
+            if (this.moves[0] == "X" && this.moves[4] == "X" && this.moves[8] == "X") {
+                xWin = true;
             }
-            if(this.moves[8] == "O"){
-                r3c3Move.innerHTML = "O";
+            if (this.moves[2] == "X" && this.moves[4] == "X" && this.moves[6] == "X") {
+                xWin = true;
+            }
+            //checks all winning combinations for O
+            if (this.moves[0] == "O" && this.moves[1] == "O" && this.moves[2] == "O") {
+                oWin = true;
+            }
+            if (this.moves[3] == "O" && this.moves[4] == "O" && this.moves[5] == "O") {
+                oWin = true;
+            }
+            if (this.moves[6] == "O" && this.moves[7] == "O" && this.moves[8] == "O") {
+                oWin = true;
+            }
+            if (this.moves[0] == "O" && this.moves[3] == "O" && this.moves[6] == "O") {
+                oWin = true;
+            }
+            if (this.moves[1] == "O" && this.moves[4] == "O" && this.moves[7] == "O") {
+                oWin = true;
+            }
+            if (this.moves[2] == "O" && this.moves[5] == "O" && this.moves[8] == "O") {
+                oWin = true;
+            }
+            if (this.moves[0] == "O" && this.moves[4] == "O" && this.moves[8] == "O") {
+                oWin = true;
+            }
+            if (this.moves[2] == "O" && this.moves[4] == "O" && this.moves[6] == "O") {
+                oWin = true;
+            }
+
+            if (xWin) {
+                this.gamesWon++;
+                this.gamesPlayed++;
+                scoreGamesPlayed.innerHTML = this.gamesPlayed;
+                scoreGamesWon.innerHTML = this.gamesWon;
+                gameOver();
+                return;
+            }
+
+            if (oWin) {
+                this.gamesLost++;
+                this.gamesPlayed++;
+                scoreGamesPlayed.innerHTML = this.gamesPlayed;
+                scoreGamesLost.innerHTML = this.gamesLost;
+                gameOver();
+                return;
+            }
+
+            let fullSquares = 0;
+            for (let i = 0; i < this.moves.length; i++) {
+                if (this.moves[i] !== "") {
+                    fullSquares++;
+                }
+            }
+
+            if (fullSquares == 9) {
+                this.gamesPlayed++;
+                scoreGamesPlayed.innerHTML = this.gamesPlayed;
+                gameOver();
+                return;
             }
         }
     }
-    let yourTurn = true;
-    game = new TicTacToe(["", "", "", "", "", "", "", "", ""], 0, 0, 0);
+
+    let xWin = false;
+    let oWin = false;
+    game = new TicTacToe(["", "", "", "", "", "", "", "", ""], 0, 0, 0, true);
 
     const r1c1Move = document.getElementById("r1-c1-move");
     const r1c2Move = document.getElementById("r1-c2-move");
@@ -66,84 +142,111 @@ window.addEventListener("load", function () {
     const r3c1Move = document.getElementById("r3-c1-move");
     const r3c2Move = document.getElementById("r3-c2-move");
     const r3c3Move = document.getElementById("r3-c3-move");
+    const scoreGamesPlayed = document.getElementById("games-played");
+    const scoreGamesWon = document.getElementById("games-won");
+    const scoreGamesLost = document.getElementById("games-lost");
+
+    function newGame() {
+        setTimeout(() => {
+            game = new TicTacToe(["", "", "", "", "", "", "", "", ""], game.gamesPlayed, game.gamesWon, game.gamesLost, true);
+            game.checkMoves();
+            xWin = false;
+            oWin = false;
+            makeNewGame = false;
+        }, 3000);
+    }
+
+    function gameOver() {
+        makeNewGame = true;
+        newGame();
+    }
 
     document.getElementById("r1-c1").addEventListener("click", function () {
-        if (yourTurn) {
+        if (!game.yourTurn || xWin || oWin) return;
+        if (game.yourTurn) {
             game.makeMove(0);
             r1c1Move.innerHTML = "X";
-            yourTurn = false;
+            game.yourTurn = false;
             game.computerMove();
         }
     });
 
     document.getElementById("r1-c2").addEventListener("click", function () {
-        if (yourTurn) {
+        if (!game.yourTurn || xWin || oWin) return;
+        if (game.yourTurn) {
             game.makeMove(1);
             r1c2Move.innerHTML = "X";
-            yourTurn = false;
+            game.yourTurn = false;
             game.computerMove();
         }
     });
 
     document.getElementById("r1-c3").addEventListener("click", function () {
-        if (yourTurn) {
+        if (!game.yourTurn || xWin || oWin) return;
+        if (game.yourTurn) {
             game.makeMove(2);
             r1c3Move.innerHTML = "X";
-            yourTurn = false;
+            game.yourTurn = false;
             game.computerMove();
         }
     });
 
     document.getElementById("r2-c1").addEventListener("click", function () {
-        if (yourTurn) {
+        if (!game.yourTurn || xWin || oWin) return;
+        if (game.yourTurn) {
             game.makeMove(3);
             r2c1Move.innerHTML = "X";
-            yourTurn = false;
+            game.yourTurn = false;
             game.computerMove();
         }
     });
 
     document.getElementById("r2-c2").addEventListener("click", function () {
-        if (yourTurn) {
+        if (!game.yourTurn || xWin || oWin) return;
+        if (game.yourTurn) {
             game.makeMove(4);
             r2c2Move.innerHTML = "X";
-            yourTurn = false;
+            game.yourTurn = false;
             game.computerMove();
         }
     });
 
     document.getElementById("r2-c3").addEventListener("click", function () {
-        if (yourTurn) {
+        if (!game.yourTurn || xWin || oWin) return;
+        if (game.yourTurn) {
             game.makeMove(5);
             r2c3Move.innerHTML = "X";
-            yourTurn = false;
+            game.yourTurn = false;
             game.computerMove();
         }
     });
 
     document.getElementById("r3-c1").addEventListener("click", function () {
-        if (yourTurn) {
+        if (!game.yourTurn || xWin || oWin) return;
+        if (game.yourTurn) {
             game.makeMove(6);
             r3c1Move.innerHTML = "X";
-            yourTurn = false;
+            game.yourTurn = false;
             game.computerMove();
         }
     });
 
     document.getElementById("r3-c2").addEventListener("click", function () {
-        if (yourTurn) {
+        if (!game.yourTurn || xWin || oWin) return;
+        if (game.yourTurn) {
             game.makeMove(7);
             r3c2Move.innerHTML = "X";
-            yourTurn = false;
+            game.yourTurn = false;
             game.computerMove();
         }
     });
 
     document.getElementById("r3-c3").addEventListener("click", function () {
-        if (yourTurn) {
+        if (!game.yourTurn || xWin || oWin) return;
+        if (game.yourTurn) {
             game.makeMove(8);
             r3c3Move.innerHTML = "X";
-            yourTurn = false;
+            game.yourTurn = false;
             game.computerMove();
         }
     });
